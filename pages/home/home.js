ebb82e3f30d19e1c3ba792ead584eaefaff3210d
17839193842 <1924141945@qq.com>
2019-04-09 17:19:53 +0800

    Mock模拟数据

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,28 @@
+//获取应用实例
+var app = getApp()
+var API = require('../../utils/api.js')
 Page({
-  
+  data: {
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    // 使用 Mock
+    API.ajax('', function (res) {
+      //这里既可以获取模拟的res
+      console.log(res)
+      that.setData({
+        list: res.data
+      })
+    });
+
+    console.log(this.data.list)
+  },
+  goBookDetail: function (e) { 
+    var bookId = e.currentTarget.dataset.bookid;         console.log(bookId); 
+    wx.navigateTo({ 
+      url: '../detail/detail?id='+bookId
+     }) 
+  }
+
 })
---
+++ utils/api.js
@@ -0,0 +1,38 @@
+let API_HOST = "http://xxx.com/xxx";
+let DEBUG = true;//切换数据入口
+var Mock = require('mock.js')
+function ajax(data = '', fn, method = "get", header = {}) {
+  if (!DEBUG) {
+    wx.request({
+      url: config.API_HOST + data,
+      method: method ? method : 'get',
+      data: {},
+      header: header ? header : { "Content-Type": "application/json" },
+      success: function (res) {
+        fn(res);
+      }
+    });
+  } else {
+    // 模拟数据
+    var res = Mock.mock({
+      'error_code': '',
+      'error_msg': '',
+      'data|10': [{
+        'id|+1': 1,
+        'name': "@cname()",
+        'img': "@image(200x300, #abcdef)",
+        'title': '@ctitle(3,8)',
+        'text': '@cparagraph(30)',
+        'address': "@county(true)",
+        'num': '@integer(0,100)',//库存数量  
+        'price': '@integer(100,2000)'//现价，单位：元
+      }]
+    })
+    // 输出结果
+    // console.log(JSON.stringify(res, null, 2))
+    fn(res);
+  }
+}
+module.exports = {
+  ajax: ajax
+}
