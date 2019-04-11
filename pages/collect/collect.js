1d1fa181b3ef888e67cf7392ca30c90131113775
17839193842 <1924141945@qq.com>
2019-04-11 14:57:56 +0800

    首页搜索(过滤法)功能

--- pages/collect/collect.js
+++ pages/collect/collect.js
@@ -1,3 +1,51 @@
+//获取应用实例
+var app = getApp()
+var API = require('../../utils/api.js')
 Page({
+  data: {
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    // 使用 Mock
+    API.ajax('', function (res) {
+      //这里既可以获取模拟的res
+      console.log(res)
+      that.setData({
+        list: res.data
+      })
+    });
+
+  },
+  // 删除
+  delShop: function (e) {
+    var that = this;
+    wx.showModal({
+      title: '提示',
+      content: '确认要删除？',
+      confirmText: '确认',
+      cancelText: '取消',
+      success: function (res) {
+        if (res.confirm) {
+          console.log("确认");
+          var index = e.target.dataset.i;
+          var cartList = that.data.list;
+          for (var i = 0; i < cartList.length; i++) {
+            if (cartList[i].id == index) {
+              cartList.splice(i, 1);
+            }
+          }
+          console.log(cartList)
+          that.setData({
+            list: cartList
+          })
+
+        } else if (res.cancel) {
+          console.log('用户点击取消删除操作')
+        }
+      }
+    })
+  }
   
+
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -3,6 +3,7 @@ var app = getApp()
 var API = require('../../utils/api.js')
 Page({
   data: {
+    searchValue: ''
   },
   onLoad: function () {
     console.log('onLoad')
@@ -23,6 +24,60 @@ Page({
     wx.navigateTo({ 
       url: '../detail/detail?id='+bookId
      }) 
+  },
+  // 搜索
+  search:function(e){
+    console.log(e)
+    var that=this;
+    var key;
+    if (e.detail.hasOwnProperty('value')){
+      key = e.detail.value;
+    }else{
+      key = e.currentTarget.dataset.val;
+    }
+    var valueList = this.data.list;
+    var arr = [];
+    console.log(key+'key')
+    if(key!=''){
+      for (let i in valueList) {
+        if (valueList[i].title.indexOf(key) >= 0) {//查找
+          arr.push(valueList[i])
+        }
+      }
+      if (arr.length == 0) {
+        that.setData({
+          list: []
+        })
+      } else {
+        that.setData({
+          list: arr//在页面显示找到的数据
+        })
+      }
+    }else{
+      wx.showToast({ // 显示Toast
+        title: '搜索字段不能为空',
+        icon: 'success',
+        duration: 2000
+      })
+    }
+    
+    console.log(this.data.list)
+  },
+  // 搜索字段设置
+  inputValue:function(e){
+    var that=this;
+    this.setData({
+      searchValue: e.detail.value
+    })
+    if (this.data.searchValue == '') {
+      API.ajax('', function (res) {
+        //这里既可以获取模拟的res
+        console.log(res)
+        that.setData({
+          list: res.data
+        })
+      });
+    }
   }
 
 })
